12b3a772ac79d9a616620b3608b6263995eee969
dlrrl <ikkee159@gmail.com>
2026-04-16 16:53:48 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import PartnerPage from './pages/PartnerPage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/partner" element={<PartnerPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
